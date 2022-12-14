794f4ddf6508bf0bee0c2fc8b11ac65c0882b909
Giovanni Grano <me@giograno.com>
2022-12-14 15:31:13 +0100

    Gio's changes

---
+++ localstack/services/acm/models.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+
+from localstack.services.stores import (
+    AccountRegionBundle,
+    BaseStore,
+    CrossRegionAttribute,
+    LocalAttribute,
+)
+
+class AcmDummyStore(BaseStore):
+
+    attribute_one: Dict[str, List[Dict]] = LocalAttribute(default=dict)
+    attribute_two: Dict[str, List[Dict]] = LocalAttribute(default=dict)
+
+
+acm_stores = AccountRegionBundle("acm", AcmDummyStore)
